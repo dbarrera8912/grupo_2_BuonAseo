d72dfde249e269e45f0cd46a88a146ceec7776b4
Sabbadini10 <matiassabbadini24@gmail.com>
2022-07-16 16:08:28 -0300

    arregle archivo app2

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3030;
-const publicPath = path.resolve(__dirname, './public');
 
-app.use(express.static(publicPath))
+app.use(express.static('public'))
 
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running in http://localhost:${port}`);
     });
 
     app.get('/', (req, res) => {
