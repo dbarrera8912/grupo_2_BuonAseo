fb987e24849c8be68f8f77ed474ac767954fdfe6
Richona <105828822+Richona@users.noreply.github.com>
2022-08-24 22:29:53 -0300

    validaciones en medotosAdd completadas

--- src/validators/val_Footer/metodosAddValidator.js
+++ src/validators/val_Footer/metodosAddValidator.js
@@ -1,6 +1,12 @@
 const {check} = require("express-validator")
 
 module.exports = [
+    check("icono")
+        .notEmpty().withMessage("Debes ingresar un icono de fontAwesone").bail(),
     check("title")
-        .notEmpty()
+        .notEmpty().withMessage("Debes ingresar un subtitulo").bail()
+        .isLength({min : 5,max : 20}).withMessage('El subtitulo debe tener entre 5 y 20 caracteres'),
+    check("letraAbajoS")
+        .notEmpty().withMessage("Debes ingresar una breve descripcion").bail()
+        .isLength({min : 5,max : 50}).withMessage('La descripcion debe tener entre 5 y 50 caracteres'),
 ]
