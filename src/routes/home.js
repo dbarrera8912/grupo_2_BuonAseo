ad4f259f3de111c1839bec5b615b51365d87497d
Richona <105828822+Richona@users.noreply.github.com>
2022-09-09 15:39:50 -0300

    agregue MD a las rutas de products

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const { home, ofertas, destacados }=require('../controllers/homeController')
+const { home, ofertas, destacados, notAdmin }=require('../controllers/homeController')
 
 /* GET home page. */
 router
       .get('/', home)
       .get('/ofertas', ofertas)
       .get('/productosDestacados', destacados)
+      .get('/notAdmin', notAdmin)
 
 module.exports = router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -7,6 +7,7 @@ const {uploadProducts} = require("../middlewares/mw_products/uploadProducts")
 const validacionesProducts = require("../validators/val_products/productsValidator")
 const guestMiddleware = require("../middlewares/mw_users/guestMiddleware")/* Middleware para no permitir ingresar a vistas si estamos logueado */
 const authMiddleware = require("../middlewares/mw_users/authMiddleware")/* Middleware para no permitir ingresar a vistas si no estamos logueado */
+const adminMiddleware = require("../middlewares/mw_users/adminMiddleware")/* Middleware para no permitir ingresar a vistas si no eres admin */
 
 // ************ Controller Require ************
 const { carrito, crearProducto, detalle, editarProducto,products,store, modificarProducto, destroy} = require('../controllers/productsController');
@@ -16,10 +17,10 @@ router
       .get('/carrito', authMiddleware, carrito)
       .get('/detail/:id', detalle)
       .get('/catalogo', products)
-      .get('/crearProducto', crearProducto)
+      .get('/crearProducto',authMiddleware, adminMiddleware, crearProducto)
       .post('/crearProducto', uploadProducts.single("imagen"), validacionesProducts, store)
-      .get('/editarProducto/:id', editarProducto)
+      .get('/editarProducto/:id', authMiddleware, adminMiddleware, editarProducto)
       .put('/modificarProducto/:id', uploadProducts.single("imagen"), validacionesProducts, modificarProducto)
-      .delete('/eliminarProducto/:id', destroy)
+      .delete('/eliminarProducto/:id', authMiddleware, adminMiddleware, destroy)
 
 module.exports = router;
