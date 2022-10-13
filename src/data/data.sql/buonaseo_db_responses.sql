ea9904d0469295ba701d9c55a5e9b527950b284f
Richona <105828822+Richona@users.noreply.github.com>
2022-10-13 16:50:35 -0300

    agregue data.sql y DER actualizado

---
+++ src/data/data.sql/buonaseo_db_responses.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: buonaseo_db
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `responses`
+--
+
+LOCK TABLES `responses` WRITE;
+/*!40000 ALTER TABLE `responses` DISABLE KEYS */;
+INSERT INTO `responses` VALUES (1,'Es simple y rápido, solo tenés que ingresar al siguiente link y segui los pasos!',1,'2022-10-13 18:30:21',NULL,NULL),(2,'Para ver los precios es fundamental registrarte como cliente, si ya lo hiciste y aun no los podes ver, aguarda la llamada de un representante BuonAseo, o comunicate con nosotros.',2,'2022-10-13 18:30:21',NULL,NULL),(3,'Si! Hacemos envíos a todos el país, ingresa y entérate de todo',3,'2022-10-13 18:30:21',NULL,NULL),(4,'El monto mínimo para retiro en tienda es de $10000, y el monto mínimo de compra para envíos es de $40000.',4,'2022-10-13 18:30:21',NULL,NULL),(5,'Envíos al interior del país: Pago anticipado por depósito en efectivo o transferencia bancaria previo al envío de la mercadería',5,'2022-10-13 18:30:21',NULL,NULL),(6,'Entregas directas: Contado contra entrega de la mercadería o anticipado vía deposito | transferencia bancaria.',5,'2022-10-13 18:30:21',NULL,NULL),(7,'Compra o retiro en mostrador: Contado en efectivo por caja. Tarjeta debito: Cabal, Visa y Mastercard. Anticipado vía deposito | transferencia bancaria.',5,'2022-10-13 18:30:21',NULL,NULL),(8,'Vení a conocer nuestro showroom! Encontrarás todos los productos que comercializamos en exhibición! Los pedidos deben realizarse por la página web donde encontrarás publicados todos nuestros productos; Una vez que hayas completado tu compra nos comunicaremos con vos para coordinar día y horario de retiro en tienda ubicada en ',6,'2022-10-13 18:30:21',NULL,NULL),(9,'Si, dependiendo del artículo que quieras comprar. En la web, debajo de la foto del artículo podes encontrar predeterminada la cantidad que viene en el pack o bulto cerrado. Hay ciertos productos, como por ejemplo: esponjas de cocina, trapos de piso y papel higiénico entre otros; que obligatoriamente se deben comprar por un mínimo determinado de unidades.',7,'2022-10-13 18:30:21',NULL,NULL),(10,'Envios: Una vez realizado el pedido, deberás tener en cuenta que será preparado y controlado por orden de llegada; Luego nos comunicaremos contigo para informarte los detalles del mismo y avanzar sobre la facturación y te enviaremos nuestros datos bancarios para que puedas realizar el pago. Al momento de recibir el comprobante de pago y su acreditación bancaria el pedido será despachado.',8,'2022-10-13 18:30:21',NULL,NULL),(11,'Retiro en tienda: Para venir a retirar tu compra a BuonAseo el procedimiento es similar a los envíos. Cuando tu pedido este preparado y controlado nos pondremos en contacto contigo para comunicarte detalles del mismo, así como también coordinar día y horario de retiro en tienda. Luego se realizará la facturación y posteriormente el pago y retiro de mercadería.',8,'2022-10-13 18:30:21',NULL,NULL),(12,'hola pa.',9,'2022-10-13 18:30:21',NULL,NULL),(13,'fadsddf',10,'2022-10-13 18:30:21',NULL,NULL),(14,'dsadds  ',11,'2022-10-13 18:30:21',NULL,NULL);
+/*!40000 ALTER TABLE `responses` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-13 15:55:51
