c06c005b6a8191299fc9f77d574d118f90a61be8
Richona <105828822+Richona@users.noreply.github.com>
2022-11-22 20:10:11 -0300

    subiendo cambios para que lo vea la crack

--- public/javascripts/form-login.js
+++ public/javascripts/form-login.js
@@ -10,3 +10,16 @@ function password_visible(tipo,element){
         document.querySelector("[name='password']").setAttribute("type","password");
     }
 }
+
+function password_visible2(tipo,element){
+    if(tipo == "mostrar"){
+        element.classList.remove("active");
+        document.querySelector(".fa-eye").classList.add("active");
+        document.querySelector("[name='password2']").setAttribute("type","text");
+    }
+    if(tipo == "ocultar"){
+        element.classList.remove("active");
+        document.querySelector(".fa-eye-slash").classList.add("active2");
+        document.querySelector("[name='password2']").setAttribute("type","password2");
+    }
+}
