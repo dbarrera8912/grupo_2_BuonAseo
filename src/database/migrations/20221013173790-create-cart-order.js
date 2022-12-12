ee2e5219f607f590ba5f405eb5edee4d25a7dfbc
Richona <105828822+Richona@users.noreply.github.com>
2022-12-12 20:32:30 -0300

    agregue migraciones del carrito

---
+++ src/database/migrations/20221013173790-create-cart-order.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cart_orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      total:{
+        type: Sequelize.INTEGER
+      },
+      userId:{
+        type: Sequelize.INTEGER
+      },
+      status:{
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cart_orders');
+  }
+};
