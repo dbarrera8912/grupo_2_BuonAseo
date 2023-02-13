e82f553d58bfb4d5f2b239e86668bf8312ebbc61
Sabbadini10 <matiassabbadini24@gmail.com>
2023-02-13 14:41:27 -0300

    cambie archivo config

--- src/controllers/api/apiAuthController.js
+++ src/controllers/api/apiAuthController.js
@@ -1,12 +1,11 @@
-const { hashSync, compareSync } = require("bcryptjs");
 const db = require("../../database/models");
 const { Op } = require('sequelize');
 const {validationResult} = require('express-validator');
 const { sendSequelizeError, createError, createErrorExpress } = require("../../helpers");
-const { sign } = require("jsonwebtoken");
+
 
 module.exports = {
-  signUp: async (req, res) => {
+  crearMensaje: async (req, res) => {
     try {
 
      
@@ -17,49 +16,26 @@ module.exports = {
       }
       
 
-      const { name, email, password } = req.body;
+      const { name, email, message} = req.body;
 // traemos el name, el email y el password del body
 
 /* con el metodo create creamos un nuevo usuario
 y le pasamos las propiedades que vengan por body 
-y pasamos por defecto null a las que se cambiaran en update*/
-      const { id, is_admin } = await db.User.create({ 
+*/
+let id=[];
+      const mensajeCreado = await db.User.create({ 
+        id : id.find(),
         name: name && name.trim(),                               
         email: email && email.trim(),
-        password: hashSync(password.trim(), 10),
-        gender: null,
-        interests: null,
-        phone: null,
-        dni: null,
-        birthday: null,
-        nationality: null,
-        postalCode: null,
-        domicile: null,
-        city: null,
-        avatar: null,
-        id_type_user: 1,
-        is_admin: 0,
+        message: message && message.trim(),
       });
 
-      /*
-     si pasa la validacion con sign() se crea un token cuando se crea un usuario con expiracion de 1hs
-       */
-      const token = sign(
-        {
-          id,
-          is_admin,
-        },
-        process.env.SECRET_KEY_JWT,
-        {
-          expiresIn: "1h",
-        }
-      );
       /* retornamos el status y el token del usuario */
       return res.status(201).json({
         ok: true,
         status: 201,
         data: {
-          token,
+          mensajeCreado,
         },
       });
     } catch (error) { // atrapamos los errores que vengan de las validaciones y los retornamos
@@ -69,140 +45,5 @@ y pasamos por defecto null a las que se cambiaran en update*/
         errors,
       });
     }
-  },
-  signIn: async (req, res) => {
-    try {
-      let errors = await validationResult(req);
-      console.log(errors)
-      if(!errors.isEmpty()){
-        throw createErrorExpress(errors, req)
-      }
-
-      const { email, password} = req.body;
-/*validamos si el email o el password es falso y si es falso mandamos el error */
-      if (!email || !password ) {
-        throw createError(404, "Se require email y password");
-      }
-/* si pasa la validacion buscamos el usuario por email */
-      
-      let user = await db.User.findOne({
-        where: {
-          email
-        },
-      });
-
-      /*   if(!user){
-                    throw createError(401, 'El usuario no se encuentra registrado');
-                }
-                if(!compareSync(password, user.password)){
-                    throw createError(401, 'La contraseña es incorrecta');
-                } */
-
-                /* validamos si el usuario o el password son falsos y mandamos el error */
-      if (!user || !compareSync(password, user.password)) {
-        throw createError(401, "Credenciales inválidas");
-      }
-
-      /*
-      si pasa la validacion con sign() se crea un token cuando se crea un usuario con expiracion de 1hs
-       */
-      const token = sign(
-        {
-          id: user.id,
-          is_admin: user.is_admin,
-        },
-        process.env.SECRET_KEY_JWT,
-        {
-          expiresIn: "1h",
-        }
-      );
-    /* retornamos el status y el token del usuario */
-      return res.status(200).json({
-        ok: true,
-        status: 200,
-        data: token,
-        user
-      });
-    } catch (error) { // atrapamos los errores que vengan de las validaciones y los retornamos
-      let errors = sendSequelizeError(error);
-      return res.status(error.status || 500).json({
-        ok: false,
-        errors,
-      });
-    }
-  },
-  verifyEmail: async (req,res) => {
-   
-    try {
-      const {email} = await req.body;
-      
-      let user = await db.User.findOne({
-        where: {
-          email
-        },
-      });
-    
-      return res.status(200).json({
-        ok: true,
-        verified: user ? true : false,
-        user
-      });
-    } catch (error) {
-      let errors = sendSequelizeError(error);
-      return res.status(error.status || 500).json({
-        ok: false,
-        errors,
-      });
-    }
-  },
-
-
-  verifyName: async (req,res) => {
-   
-    try {
-      const {name} = await req.body;
-      
-      let user = await db.User.findOne({
-        where: {
-          name
-        },
-      });
-      
-      return res.status(200).json({
-        ok: true,
-        verified: user ? true : false
-      });
-    } catch (error) {
-      let errors = sendSequelizeError(error);
-      return res.status(error.status || 500).json({
-        ok: false,
-        errors,
-      });
-    }
-  },
-  verifyPassword: async (req,res) => {
-    try {
-      const {email} = await req.body;
-      
-      let user = await db.User.findOne({
-        where: {
-         email
-        },
-      });
-
-      let pass = compareSync(req.body.password, user.password)
-      
-      
-      return res.status(200).json({
-        ok: true,
-        verifica: pass ? true : false,
-      });
-    } catch (error) {
-      let errors = sendSequelizeError(error);
-      return res.status(error.status || 500).json({
-        ok: false,
-        errors,
-      });
-    }
-
-}}
+  }, 
+}
